a6e0aadad5a8a0deb1f6c1441bbff2edecef3fa9
Jorge B <George17zenit@outlook.com>
2023-07-13 16:28:16 -0600

    Agrega inspector de argumentos y Sleep con argv[1]

--- HelloJorge/HelloJorge.cpp
+++ HelloJorge/HelloJorge.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <windows.h>
 
-int main()
+int main(int argc, char** argv)
 {
+    //Informar cuantos argumentos se enviaron al programa
+    std::cout << "N de argumentos: " << argc << std::endl;
+    for (int i = 0; i < argc; i++)
+    {
+        std::cout << "argv[" << i << "] : " << argv[i] << std::endl;
+    }
+    Sleep(atoi (argv[1]));
     std::cout << "Hello Jorge!\n";
 }
