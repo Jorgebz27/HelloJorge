28630ba92c45c88041d39f105050673f1a8bde8f
Jorge B <George17zenit@outlook.com>
2023-07-17 15:55:38 -0600

    Sitch case del ejemplo de overflow

--- HelloJorge/HelloJorge.cpp
+++ HelloJorge/HelloJorge.cpp
@@ -4,11 +4,33 @@
 int main(int argc, char** argv)
 {
     //Informar cuantos argumentos se enviaron al programa
-    std::cout << "N de argumentos: " << argc << std::endl;
+    /*std::cout << "N de argumentos: " << argc << std::endl;
     for (int i = 0; i < argc; i++)
     {
         std::cout << "argv[" << i << "] : " << argv[i] << std::endl;
     }
     Sleep(atoi (argv[1]));
-    std::cout << "Hello Jorge!\n";
-}
+    std::cout << "Hello Jorge!\n";*/
+
+    byte karma;
+
+    std::cout << "Karma: " << karma << "\n";
+
+    for (int i = 0; i < 20; i++)
+    {
+        karma += 10;
+        switch (karma)
+        {
+
+        }
+    }
+
+   
+    
+
+
+
+
+
+    return 0;
+} 
