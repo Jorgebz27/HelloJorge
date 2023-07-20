dffef7690afb6a425c70c5fb5e40c24c78c370e3
Jorge B <George17zenit@outlook.com>
2023-07-19 20:30:11 -0600

    Karma finalizado

--- HelloJorge/HelloJorge.cpp
+++ HelloJorge/HelloJorge.cpp
@@ -12,25 +12,36 @@ int main(int argc, char** argv)
     Sleep(atoi (argv[1]));
     std::cout << "Hello Jorge!\n";*/
 
-    byte karma;
+    byte karma = 128;
 
     std::cout << "Karma: " << karma << "\n";
 
     for (int i = 0; i < 20; i++)
     {
         karma += 10;
-        switch (karma)
-        {
+        std::cout << "incrementando karma en 10: " << (int)karma << " ";
 
+        if (karma == 0 || karma < 35)
+        {
+            std::cout << "Muy malo\n";
+        }
+        else if (karma == 35 || karma < 95)
+        {
+            std::cout << "Malo normal\n";
+        }
+        else if (karma == 95 || karma < 115)
+        {
+            std::cout << "Neutro\n";
+        }
+        else if (karma == 115 || karma < 200)
+        {
+            std::cout << "Bueno\n";
+        }
+        else
+        {
+            std::cout << "Muy bueno\n";
         }
     }
 
-   
-    
-
-
-
-
-
     return 0;
-} 
+}
